140178e978e58e7912fa98e444110e60ed31363d
Cortnie <cortniemuscari@gmail.com>
2022-04-13 12:50:10 -0500

    manager object

--- index.js
+++ index.js
@@ -8,13 +8,81 @@ const Intern = require('./lib/Intern');
 
 
 // start function
-const startRoster = function () {
-    console.log("-------------------------------------------------------------------------");
-    console.log("PLEASE START YOUR ROSTER BY ENTERING THE TEAM MANAGER'S INFORMATION FIRST");
-    console.log("-------------------------------------------------------------------------");
-    
-    new Manager().getName();
+const startRoster = () => {
+    console.log("--------------------------------");
+    console.log("START CREATING YOUR TEAM ROSTER!");
+    console.log("--------------------------------");
 };
+
+
+// prompt user to add another team member function
+const promptAddMember = () => {
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'confirmAdd',
+            message: 'Would you like to add a new team member to your roster?',
+            default: true
+        },
+        {
+            type: 'list',
+            name: 'addTeamMember',
+            message: 'Please select what type of team member you would like to add:',
+            choices: ['Engineer', 'Intern'],
+            when: ({ confirmAdd }) => {
+                if (confirmAdd) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+    ])
+};
+
+
+// prompt user for manager info function
+const promptManagerInfo = managerInfo => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the Team Manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the Team Manager's ID number?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Team Manager's email address?",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the Team Manager's office number?",
+        },
+    ]);
+    promptAddMember();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -24,6 +92,10 @@ const startRoster = function () {
 
 // call start function
 startRoster();
+promptManagerInfo()
+    .then(managerInfo => {
+        return
+    })
 
 
 
@@ -56,3 +128,48 @@ startRoster();
 //     console.log(err);
 //   });
 
+
+
+// prompt user for profile questions
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    ]);
+};
